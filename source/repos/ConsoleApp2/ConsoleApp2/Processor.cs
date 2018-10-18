8eb28412d0551525f9994553824d5742513d3be6
TheNutBoy <vladyavorsky478@gmail.com>
2018-10-18 20:03:34 +0500

    Test2

---
+++ source/repos/ConsoleApp2/ConsoleApp2/Processor.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ConsoleApp2
+{
+    interface IProcessor
+    {
+        void Finish();
+        void Process(int i);
+    }
+    class Processor : IProcessor
+    {
+        public int Index;
+        public Processor()
+        {
+            Logger.Log.Info("Процессор создан");
+        }
+        public void Finish()
+        {
+            Logger.Log.Info($"Finishing Process({Index})");
+        }
+
+        public void Process(int i)
+        {
+            Index = i;
+            Logger.Log.Info($"Processing by ({Index})...");
+        }
+    }
+}
---
+++ source/repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using log4net;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Processor> procs = new List<Processor>();
+            Logger.InitLogger();
+            for (int i = 0; i < 10; i++)
+            {
+                procs.Add(new Processor());
+                procs[i].Process(i);
+            }
+
+            foreach (var proc in procs)
+            {
+                proc.Finish();
+            }
+        }
+    }
+}
